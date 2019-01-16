3493eac8fb2cb6c5855cabb5944ce20668495fc9
liudong0914 <947390587@qq.com>
2019-01-16 10:28:05 +0800

    java类微调

--- src/main/java/HelloServlet.java
+++ src/main/java/HelloServlet.java
@@ -7,7 +7,7 @@ public class HelloServlet extends javax.servlet.http.HttpServlet {
     }
 
     protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
-        response.getWriter().print("<h1>Hello Servlet!!</h1>");
+        response.getWriter().print("<h1>Hello Servlet!!!</h1>");
         response.getWriter().print(new Date().toLocaleString());
     }
 }
